05adc95abd268a815e3b51cca89f0ec36923e22d
Amit <amitsamant@gmail.com>
2023-08-18 10:02:14 +0530

    some comments

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import threading
 
 def handle_client(conn, msg):
     with conn:
-        conn.recv(1024)
+        conn.recv(1024)  # receiving 1024 bytes
         conn.send(msg.encode())
         while True:
             message = conn.recv(1024)
@@ -26,6 +26,7 @@ def main():
     while True:
         conn, addr = server_socket.accept()
         msg = "+PONG\r\n"
+        # for every new connection we get, we create a thread
         thread = threading.Thread(target=handle_client, args=(conn, msg))
         thread.start()
 
